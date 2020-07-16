564ffeaee2b8c03cf003a54abd367ffcecba3762
Josh Montoya <josh@montoya.io>
2020-07-16 10:58:02 -0700

    implement relationships

--- entry.go
+++ entry.go
@@ -8,4 +8,16 @@ import (
 type Entry struct {
 	// Include dbl.Entry to auto-populate fields/methods needed to match the
 	dbl.Entry
+
+	// UserID which Entry is related to, used a relational referenced
+	UserID string `json:"userID"`
+}
+
+// GetRelationshipIDs will return the relationship IDs associated with the Entry
+func (e *Entry) GetRelationshipIDs() (ids []string) {
+	// UserID is our only relationship at the moment. Any number of relationships can
+	// be set for entries. Just know that each added relationship is an extra reference
+	// to be managed by DBL and stored by the underlying back-end.
+	ids = append(ids, e.UserID)
+	return
 }
