454900b3574802eb03bdcf758ce8f981d8d5ec9e
Josh Montoya <josh@montoya.io>
2020-07-22 16:46:14 -0700

    add err output to ForEach

--- controller.go
+++ controller.go
@@ -90,7 +90,7 @@ func (c *Controller) GetByUser(userID string) (entries []*Entry, err error) {
 // Note: The error constant dbl.Break can returned by the iterating func to end the iteration early
 func (c *Controller) ForEach(fn func(*Entry) error) (err error) {
 	// Iterate through all entries
-	c.c.ForEach(func(key string, val dbl.Value) (err error) {
+	err = c.c.ForEach(func(key string, val dbl.Value) (err error) {
 		// Attempt to assert the value as an *Entry
 		e, ok := val.(*Entry)
 		// Ensure assertion was successful
